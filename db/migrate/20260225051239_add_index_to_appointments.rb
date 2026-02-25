d0a493a467f8039a6be4e83a7ba0a6d443957f57
Iksha Labs <ikshalabs@Sheerins-MacBook-Pro.local>
2026-02-25 11:43:10 +0530

    Fix Rubocop lint errors

--- db/migrate/20260225051239_add_index_to_appointments.rb
+++ db/migrate/20260225051239_add_index_to_appointments.rb
@@ -1,6 +1,6 @@
 class AddIndexToAppointments < ActiveRecord::Migration[8.1]
   def change
-    add_index :appointments, [:start_time, :end_time]
+    add_index :appointments, [ :start_time, :end_time ]
     add_index :appointments, :status
   end
 end
